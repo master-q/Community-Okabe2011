42b6e09b5720907b73f0cea6c23f1ef8f88aec10
Kiwamu Okabe <kiwamu@debian.or.jp>
2011-06-09 23:21:22 +0900

    smart!

--- Projects/number/number.hs
+++ Projects/number/number.hs
@@ -1,17 +1,10 @@
 import Prelude
 import Control.Monad.State
 
-numState :: State Int Int -- カウンタ制御がここに集中してる
-numState = do x <- get
-              put (x + 1)
-              return (x + 1)
-
-addNumber :: Int -> [String] -> [String] -- これで、、、いいわけない、、、
-addNumber n (l:ls) = [show a ++ ": " ++ l] ++ addNumber s ls
-                     where (a, s) = runState numState n
-addNumber _ [] = []
+addNumber :: String -> State Int String
+addNumber l = do modify (+1)
+                 x <- get
+                 return (show x ++ ": " ++ l)
 
 main :: IO ()
-main = do
-  x <- getContents
-  putStr $ unlines $ addNumber 0 $ lines x
+main = interact $ unlines . flip evalState 0 . mapM addNumber . lines
